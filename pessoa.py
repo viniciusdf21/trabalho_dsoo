269adeed3557d7096eba308d4efaf3606e99b994
Nicole <mottanicolerodrigues@gmail.com>
2026-06-01 17:33:18 -0300

    feat: Implement properties and setters in pessoa.py
    Added properties and setters for nome, cpf, and celular attributes.

--- pessoa.py
+++ pessoa.py
@@ -6,3 +6,35 @@ class Pessoa(ABC):
         self.__nome = nome
         self.__cpf = cpf
         self.__celular = celular
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+        
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @cpf.setter
+    def cpf(self, novo_cpf):
+        self.__cpf = novo_cpf
+        
+    @property
+    def celular(self):
+        return self.__celular
+    
+    @celular.setter
+    def celular(self, novo_celular):
+        self.__celular = novo_celular
+        
+    def validar_cpf():
+        # implementacao
+        pass
+        
+    @abstractmethod
+    def exibir_dados():
+        pass
