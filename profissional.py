ecaada967c1620fdf0d7366e6c941a4ea35bb037
Nicole <mottanicolerodrigues@gmail.com>
2026-06-02 12:26:56 -0300

    feat: métodos, getters e setter

--- profissional.py
+++ profissional.py
@@ -6,3 +6,22 @@ class Profissional(Pessoa):
         super().__init__(nome, cpf, celular)
         self.__especialidade = especialidade
         self.__registro = registro
+
+    @property
+    def especialidade(self):
+        return self.__especialidade
+    
+    @especialidade.setter
+    def especialidade(self, nova_esp):
+        self.__especialidade = nova_esp
+        
+    @property
+    def registro(self):
+        return self.__registro
+    
+    @registro.setter
+    def registro(self, novo_reg):
+        self.__registro = novo_reg
+        
+    def exibir_especialidade(self):
+        return f"Especialidade: {self.__especialidade}"
