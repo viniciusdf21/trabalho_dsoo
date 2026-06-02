4cd065a96294b770461b56a9d693451a1b08ff13
Nicole <mottanicolerodrigues@gmail.com>
2026-06-02 16:15:54 -0300

    fix: data_nascimento.setter

--- paciente.py
+++ paciente.py
@@ -5,23 +5,25 @@ from datetime import date
 class Paciente(Pessoa):
     def __init__(self, nome, cpf, telefone, data_nascimento):
         super().__init__(nome, cpf, telefone)
-        self.__data_nascimento = data_nascimento
+        self.data_nascimento = data_nascimento
 
     @property
     def data_nascimento(self):
-        return self.__data_nascimento
+        return self.data_nascimento
     
     @data_nascimento.setter
     def data_nascimento(self, nova_data):
-        self.__data_nascimento = nova_data
+        if not isinstance(nova_data, date):
+            raise ValueError("A data de nascimento deve ser do tipo date.")
+        self.data_nascimento = nova_data
 
     def verificar_idade(self):
         hoje = date.today()
-        idade = hoje.year - self.__data_nascimento.year
+        idade = hoje.year - self.data_nascimento.year
         
         if (hoje.month, hoje.day) < (
-            self.__data_nascimento.month,
-            self.__data_nascimento.day
+            self.data_nascimento.month,
+            self.data_nascimento.day
         ):
             idade -= 1
             
