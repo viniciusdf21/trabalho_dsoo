bdea0492a82c833a60400ee51d3077b660b460e9
Nicole <mottanicolerodrigues@gmail.com>
2026-06-02 12:12:36 -0300

    feat: Add data_nascimento getter e setter

--- paciente.py
+++ paciente.py
@@ -5,3 +5,11 @@ class Paciente(Pessoa):
     def __init__(self, nome, cpf, telefone, data_nascimento):
         super().__init__(nome, cpf, telefone)
         self.__data_nascimento = data_nascimento
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+    
+    @data_nascimento.setter
+    def data_nascimento(self, nova_data):
+        self.__data_nascimento = nova_data
