79ef10ee16e16d31b6c0029445a2cb97dcacd116
Vinicius Fronza <vinicius.dalmarcofronza@gmail.com>
2026-06-03 14:42:09 -0300

    feat: adiciona um método para a classe 'Pix'

--- pix.py
+++ pix.py
@@ -13,3 +13,10 @@ class Pix(Pagamento):
     @cpf_pagante.setter
     def cpf_pagante(self, cpf_pagante):
         self.__cpf_pagante = cpf_pagante
+
+    def validar_cpf(self):
+        cpf = self.__cpf_pagante
+        if len(cpf) == 11:
+            return True
+        else:
+            return False
