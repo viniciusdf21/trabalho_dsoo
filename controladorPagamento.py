81e2c04922d29470698dc81962684ff979b20da8
Vinicius Fronza <vinicius.dalmarcofronza@gmail.com>
2026-06-04 10:57:14 -0300

    feat: add class ControladorPagamento

--- controladorPagamento.py
+++ controladorPagamento.py
@@ -1,1 +1,16 @@
+from pagamento import Pagamento
+
+
+class ControladorPagamento:
+    def registrar_pagamento(self, pagamento: Pagamento):
+        try:
+            pagamento.registrar_pagamento()
+            pagamento.atendimento.adicionar_pagamento(pagamento)
+
+            return pagamento
+
+        except ValueError as erro:
+            print(f"Erro ao registrar pagamento: {erro}")
+            return None
+
 
