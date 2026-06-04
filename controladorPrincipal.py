99091a33053d3f4bc20a4d42b1ee1201244a9632
Nicole <mottanicolerodrigues@gmail.com>
2026-06-04 14:30:34 -0300

    feat: opções dos controladores específicos

--- controladorPrincipal.py
+++ controladorPrincipal.py
@@ -14,10 +14,25 @@ class ControladorPrincipal:
             print("1 - Pacientes")
             print("2 - Profissionais")
             print("3 - Clínica")
-            print("4 - Atendimentos")
+            print("4 - Tipos de atendimentos")
             print("0 - Sair")
 
             opcao = input("Escolha uma opção: ")
 
-            if opcao == "0":
+            if opcao == '1':
+                self.__controladorPaciente.abrir_menu()
+                
+            elif opcao == "2":
+                self.__controlador_profissionais.abrir_menu()
+
+            elif opcao == "3":
+                self.__controlador_clinica.abrir_menu()
+
+            elif opcao == "4":
+                self.__controlador_tipo_atendimentos.abrir_menu()
+
+            elif opcao == "0":
                 break
+
+            else:
+                print("Opção inválida.")
