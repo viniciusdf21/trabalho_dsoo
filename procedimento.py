d3610a3066df79b6caaf8f400c23ba3d882b6d07
Vinicius Fronza <vinicius.dalmarcofronza@gmail.com>
2026-06-04 10:57:45 -0300

    feat: add método

--- procedimento.py
+++ procedimento.py
@@ -36,3 +36,11 @@ class Procedimento:
     @profissional.setter
     def profissional(self, profissional):
         self.__profissional = profissional
+    
+    def exibir_dados(self):
+    return (
+        f"Nome do procedimento: {self.__nome}\n"
+        f"Descrição: {self.__descricao}\n"
+        f"Custo: R$ {self.__custo:.2f}\n"
+        f"Profissional: {self.__profissional.nome}"
+    )
