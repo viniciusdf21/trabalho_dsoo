09fd3e0f93f8520b60d4c1045c9f3db103d166d6
Nicole <mottanicolerodrigues@gmail.com>
2026-06-05 18:08:38 -0300

    fix: atributo cpf

--- pessoa.py
+++ pessoa.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class Pessoa(ABC):
     def __init__(self, nome, cpf, celular):
         self.__nome = nome
-        self.__cpf = cpf
+        self.cpf = cpf
         self.__celular = celular
 
     @property
