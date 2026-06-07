7daca24d77896584eb086e9edee4cd1cf98887bb
Nicole <mottanicolerodrigues@gmail.com>
2026-06-07 19:37:10 -0300

    feat: Add TelaPaciente class with patient data methods

--- telaPaciente.py
+++ telaPaciente.py
@@ -1,1 +1,11 @@
+class TelaPaciente:
 
+    def pegar_dados_paciente(self):
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        celular = input("Celular: ")
+
+        return nome, cpf, celular
+
+    def mostrar_mensagem(self, mensagem):
+        print(mensagem)
