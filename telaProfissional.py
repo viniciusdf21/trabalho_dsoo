733782f8433e41509f7aad4def1f8c4e68bd91fc
Nicole <mottanicolerodrigues@gmail.com>
2026-06-07 20:07:47 -0300

    feat: Add TelaProfissional class with data input methods

--- telaProfissional.py
+++ telaProfissional.py
@@ -1,1 +1,20 @@
+class TelaProfissional:
 
+    def pegar_dados_profissional(self):
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        celular = input("Celular: ")
+        especialidade = input("Especialidade: ")
+        registro = input("Registro: ")
+
+        return (nome,cpf,celular,especialidade,registro)
+
+    def mostrar_menu(self):
+        print("\n=== PROFISSIONAIS ===")
+        print("1 - Cadastrar")
+        print("2 - Listar")
+        print("3 - Excluir")
+        print("4 - Alterar")
+        print("0 - Voltar")
+
+        return input("Opção: ")
