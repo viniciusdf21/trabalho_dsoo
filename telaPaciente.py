6e3d96544346329b7226dd5ecf22eff6a4f615e4
Nicole <mottanicolerodrigues@gmail.com>
2026-06-07 19:41:13 -0300

    feat: Add menu display method to telaPaciente

--- telaPaciente.py
+++ telaPaciente.py
@@ -9,3 +9,13 @@ class TelaPaciente:
 
     def mostrar_mensagem(self, mensagem):
         print(mensagem)
+
+    def mostrar_menu(self):
+        print("\n=== PACIENTES ===")
+        print("1 - Cadastrar")
+        print("2 - Listar")
+        print("3 - Excluir")
+        print("4 - Alterar")
+        print("0 - Voltar")
+
+        return input("Opção: ")
