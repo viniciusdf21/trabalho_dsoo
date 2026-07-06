a3c4521d5d54cbc29efa92dbdfe7e2c54900e567
Nicole <mottanicolerodrigues@gmail.com>
2026-07-06 12:06:14 -0300

    fix: Update method to retrieve patient data

--- controladorPaciente.py
+++ controladorPaciente.py
@@ -10,7 +10,7 @@ class ControladorPaciente:
         self.__tela = TelaPaciente()
 
     def cadastrar_paciente(self):
-        dados = self.__tela.alterar_paciente()
+        dados = self.__tela.pegar_dados_paciente()
         if dados is None:
             return
 
