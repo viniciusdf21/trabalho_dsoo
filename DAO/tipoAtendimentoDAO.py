3b51dab57dda3a0af54df9ea289f30402ce77c0b
Nicole <mottanicolerodrigues@gmail.com>
2026-07-06 01:18:35 -0300

    feat: Implement TipoAtendimentoDAO class for data handling

---
+++ DAO/tipoAtendimentoDAO.py
@@ -0,0 +1,20 @@
+from dao import DAO
+from tipoAtendimento import TipoAtendimento
+
+
+class TipoAtendimentoDAO(DAO):
+
+    def __init__(self):
+        super().__init__("tipos_atendimento.pkl")
+
+    def add(self, tipo: TipoAtendimento):
+        if isinstance(tipo, TipoAtendimento):
+            super().add(tipo.nome, tipo)
+
+    def update(self, tipo: TipoAtendimento):
+        if isinstance(tipo, TipoAtendimento):
+            super().update(tipo.nome, tipo)
+
+    def remove(self, tipo: TipoAtendimento):
+        if isinstance(tipo, TipoAtendimento):
+            super().remove(tipo.nome)
