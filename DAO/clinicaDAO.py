5f3590fee9dc772d9c0d2e391ce7ec09751d0f4c
Nicole <mottanicolerodrigues@gmail.com>
2026-07-05 21:54:16 -0300

    Implement ClinicaDAO with add, update, and remove methods

--- DAO/clinicaDAO.py
+++ DAO/clinicaDAO.py
@@ -1,1 +1,20 @@
+from dao import DAO
+from clinica import Clinica
 
+
+class ClinicaDAO(DAO):
+
+    def __init__(self):
+        super().__init__("clinicas.pkl")
+
+    def add(self, clinica: Clinica):
+        if isinstance(clinica, Clinica):
+            super().add(clinica.nome, clinica)
+
+    def update(self, clinica: Clinica):
+        if isinstance(clinica, Clinica):
+            super().update(clinica.nome, clinica)
+
+    def remove(self, clinica: Clinica):
+        if isinstance(clinica, Clinica):
+            super().remove(clinica.nome)
