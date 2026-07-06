015aa595f84fb87077e8268c8b59db06c1f597a5
Nicole <mottanicolerodrigues@gmail.com>
2026-07-06 01:14:41 -0300

    feat: Implement PacienteDAO for managing Paciente objects

--- DAO/pacienteDAO.py
+++ DAO/pacienteDAO.py
@@ -1,1 +1,20 @@
+from DAO.dao import DAO
+from paciente import Paciente
 
+
+class PacienteDAO(DAO):
+
+    def __init__(self):
+        super().__init__("pacientes.pkl")
+
+    def add(self, paciente: Paciente):
+        if isinstance(paciente, Paciente):
+            super().add(paciente.cpf, paciente)
+
+    def update(self, paciente: Paciente):
+        if isinstance(paciente, Paciente):
+            super().update(paciente.cpf, paciente)
+
+    def remove(self, paciente: Paciente):
+        if isinstance(paciente, Paciente):
+            super().remove(paciente.cpf)
